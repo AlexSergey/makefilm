497b0afc0d8fb7025d78f4802221113f3f005876
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2025-01-09 22:25:52 +0100

    feat: init monorepo

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '*.md': ['prettier --write'],
-  'package.json': ['npm run format:package'],
-  '{src,test}/**/*.ts': [() => 'npm run lint:ts'],
-  '{src,test}/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
-};
