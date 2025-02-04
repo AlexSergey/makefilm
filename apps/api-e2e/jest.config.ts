a00c0facb93f7ba96f3ac68b90924adbb5786ca5
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2025-02-04 22:37:28 +0100

    fix(ci): pipeline fixes

--- apps/api-e2e/jest.config.ts
+++ apps/api-e2e/jest.config.ts
@@ -5,6 +5,7 @@ export default {
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
   moduleFileExtensions: ['ts', 'js', 'html'],
   preset: '../../jest.preset.js',
+  runInBand: true,
   setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/api/?(*.)+(e2e-spec).[jt]s?(x)'],
--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -3,6 +3,7 @@ export default {
   displayName: 'api',
   moduleFileExtensions: ['ts', 'js', 'html'],
   preset: '../../jest.preset.js',
+  runInBand: true,
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
