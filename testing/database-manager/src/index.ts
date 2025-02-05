f2ee7837b739d21ee2624b1b6e1d9fa55f28f586
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2025-02-05 21:56:10 +0100

    fix(ci): pipeline fixes

--- testing/database-manager/src/index.ts
+++ testing/database-manager/src/index.ts
@@ -1,6 +1,8 @@
 import { dataSource as _dataSource } from 'database/src/data-source';
 import { DataSource } from 'typeorm';
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export interface DatabaseManager {
   cleanupDatabase(): Promise<void>;
 
@@ -13,9 +15,11 @@ export const databaseManager = async (): Promise<DatabaseManager> => {
   const dataSource = await _dataSource.initialize();
 
   const cleanupDatabase = async (): Promise<void> => {
+    await sleep(1000);
     const entities = dataSource.entityMetadatas;
     const tableNames = entities.map((entity) => `"${entity.tableName}"`).join(', ');
     await dataSource.query(`TRUNCATE ${tableNames} CASCADE;`);
+    await sleep(1000);
     // eslint-disable-next-line no-console
     console.log('[TEST DATABASE]: Clean');
   };
